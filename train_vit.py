9686ed1665ff9a8782e74fbc5922491c048c89c4
Tianxu Jiang <88474547+TnTerry@users.noreply.github.com>
2024-08-02 14:52:18 -0400

    Update train_vit.py

--- train_vit.py
+++ train_vit.py
@@ -1,41 +1,60 @@
-import torch
-from torch.utils.data import Dataset, DataLoader, random_split
-from torchvision import transforms
-from PIL import Image
-import numpy as np
 import os
 import json
+import prettytable as pt
+from collections import Counter
+import argparse
+from pathlib import Path
+import math
+import time
+
+from PIL import Image
+import numpy as np
+import cv2
+from tqdm import tqdm
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import models
-from MEGL.utils.utils import *
-from torch.nn.utils.rnn import pad_sequence
-from MEGL.utils.gradcam import GradCam
-from PIL import Image
-import requests
-import torch
-import torch.nn as nn
-from transformers import AutoTokenizer, AutoProcessor, CLIPModel
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader, random_split, Subset, SubsetRandomSampler
 from torchvision import transforms
-from PIL import Image
-import torch
-from torch import nn, optim
-from transformers import CLIPModel
+from torch.nn.utils.rnn import pad_sequence
+from torch.optim.lr_scheduler import LambdaLR, CosineAnnealingLR
+torch.autograd.set_detect_anomaly(True)
+from transformers import LlavaProcessor, LlavaForConditionalGeneration, CLIPModel
+from accelerate import Accelerator, DistributedType
+
+from model.utils.utils import *
+from model.utils.data import *
+from model.utils.gradcam import GradCam
+from model.vit import *
 from vit_att import *
 
-import argparse
-
-# Create the parser
-parser = argparse.ArgumentParser(description='Process the task type.')
-
-parser.add_argument('--task', type=str, default='object', choices=['action', 'object'],
-                    help='Specify the task type: "action" or "object".')
-parser.add_argument('--num_epochs', type=int, default=10,
-                    help='Specify the number of epochs for training (default: 30).')
-
-# Parse the command-line arguments
-args = parser.parse_args()
+# Create parser
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process the task type.')
+    parser.add_argument('--task', type=str, default='object', choices=['action', 'object'],
+                        help='Specify the task type: "action" or "object".')
+    parser.add_argument('--explanation_type', type=str, default="none", 
+                        choices=["multimodal", "visual", "none", "text"],
+                        help="Specify the type of explanation-guided learning (default: multimodal)")
+    parser.add_argument('--num_epochs', type=int, default=3,
+                        help='Specify the number of epochs for training (default: 3).')
+    parser.add_argument('--transformation', type=str, default="GRADIA", choices=["GRADIA", "HAICS"],
+                        help='Specify the type of transformation for the visual explanation (default: GRADIA).')
+    parser.add_argument('--batch_size', type=int, default=64, 
+                        help='Specify the batch size (default: 8).')
+    parser.add_argument('--att_weight', type=float, default=0.1,
+                        help='Weight of the attention loss term in the loss function (default: 1).')
+    parser.add_argument('--exp_weight', type=float, default=1,
+                        help='Weight of the explanation loss term in the loss function (default: 1).')
+    args = parser.parse_args()
+    return args
 
 # Get the directory path of the current script
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -48,33 +67,56 @@ def load_json_data(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
+args = parse_args()
+
 if args.task == "action":
-    data = load_json_data("Datasets/Action_Classification/exp_annotation.json")
-    root_dir = 'Datasets/Action_Classification/'  # Adjust as necessary to point to the correct root directory
-
-    id_map = load_json_data("Datasets/Action_Classification/label_class_id_mapping.json")
+    root_dir = Path('Datasets/Action_Classification/')
 elif args.task == "object":
-    data = load_json_data("Datasets/Object_Classification/exp_annotation.json")
-    root_dir = 'Datasets/Object_Classification/'  # Adjust as necessary to point to the correct root directory
-
-    id_map = load_json_data("Datasets/Object_Classification/label_class_id_mapping.json")
-else:
-    raise ValueError("Invalid task specified. Expected 'action' or 'object'.")
-
-# Load a pre-trained ResNet-50 model and modify it for your number of classes
-num_classes = len(id_map)  # Update this to your actual number of classes
+    root_dir = Path('Datasets/Object_Classification/')
 
 def custom_collate_fn(batch):
-    images = torch.stack([item['image'] for item in batch])
-    visual_exps = torch.stack([item['visual_exp'] for item in batch])
-    class_ids = torch.tensor([item['class_id'] for item in batch])
-    exps = [item['exp'] for item in batch]  # List of lists
+    human_input_lst = []
+    gpt_output_lst = []
+    image_lst = []
+    visual_exp_lst = []
+    class_id_lst = []
+    ans_lst = []
+    for item in batch:
+        human_input, gpt_output, image_path, visual_exp_path, class_id, ans = item
 
-    # If you're using tokenized indices for text and want to pad:
-    # exps_padded = pad_sequence([torch.tensor(exp) for sublist in exps for exp in sublist], 
-    #                            batch_first=True, padding_value=0)
+        human_input_lst.append(human_input)
+        gpt_output_lst.append(gpt_output)
 
-    return {'image': images, 'visual_exp': visual_exps, 'class_id': class_ids, 'exp': exps}
+        image = Image.open(image_path).convert('RGB')
+
+        if visual_exp_path:
+            visual_exp = np.load(visual_exp_path)
+            if visual_exp.ndim == 3 and visual_exp.shape[0] == 3:  # Check if it's a 3-channel image
+                visual_exp = visual_exp.transpose(1, 2, 0)  # Reorder dimensions to (height, width, channels)
+                if visual_exp.dtype == np.float64:
+                    visual_exp = (visual_exp * 255).astype(np.uint8)  # Assuming the data is in range [0, 1]
+            
+            visual_exp = Image.fromarray(visual_exp.astype('uint8'))  # Assume it's grayscale
+            visual_exp = visual_exp.convert('RGB')
+        else:
+            visual_exp = Image.new('RGB', image.size, (0, 0, 0)) # Dummy image of zeros
+        
+        image = transform_image(image)
+        image_lst.append(image)
+
+        visual_exp = transform_visualexp(visual_exp)
+        visual_exp_lst.append(visual_exp)
+
+        class_id = torch.tensor(class_id, dtype=torch.long)
+        class_id_lst.append(class_id)
+
+        ans_lst.append(ans)
+    
+    images = torch.stack(image_lst)
+    visual_exps = torch.stack(visual_exp_lst)
+    class_ids = torch.tensor(class_id_lst)
+
+    return human_input_lst, gpt_output_lst, images, visual_exps, class_ids, ans_lst
 
 transform_image = transforms.Compose([
                   transforms.Resize((224, 224)),
@@ -87,91 +129,188 @@ transform_visualexp = transforms.Compose([
                       transforms.ToTensor()
                       ]) 
 
-# Assuming CustomDataset is defined somewhere
-dataset = MMESDataset(data, root_dir, transform_image, transform_visualexp)
+dataset = MEGLDataset(root_dir)
 
-# Split the dataset into train and test sets
+# Adjust the num_classes based on the actual dataset
+class_id_lst = [int(dataset[i][4]) for i in range(len(dataset))]
+class_id_cnt = Counter(class_id_lst)
+print("Acutal Dataset Label:\n", class_id_cnt)
+num_classes = len(class_id_cnt)
+
 train_size = int(0.8 * len(dataset))
 test_size = len(dataset) - train_size
 train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
-# DataLoaders for train and test datasets
-train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, collate_fn=custom_collate_fn)
-test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False, collate_fn=custom_collate_fn)
+train_loader = DataLoader(train_dataset, batch_size=64, collate_fn=custom_collate_fn)
+test_loader = DataLoader(test_dataset, batch_size=64, collate_fn=custom_collate_fn)
 
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch16")
+clip_vit = clip_model.vision_model
+model = MEGL_ViT(args.explanation_type, clip_vit, num_classes)
 
-
-clip_vision_transformer = model.vision_model  # Assuming you have the original CLIPVisionTransformer model defined
-# Create an instance of the classifier model
-model_cls = CLIPVisionTransformerClassifier(clip_vision_transformer, num_classes)
-
-# Assuming you have already set up the model_cls from the provided class definition
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model_cls.to(device)
+model = model.to(device)
+
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model_cls.parameters(), lr=1e-4)
+optimizer = optim.AdamW(model.parameters(), lr=1e-4)
 
+def lr_lambda(epoch):
+    warmup_epochs = 10
+    if epoch < warmup_epochs:
+        return epoch / warmup_epochs
+    else:
+        return 0.5 * (1 + math.cos((epoch - warmup_epochs) * math.pi / (50 - warmup_epochs)))
 
-def train(model, dataloader, optimizer, criterion, device):
+scheduler = LambdaLR(optimizer, lr_lambda)
+
+# model.eval()
+# correct = 0
+# total = 0
+# total_label_tuned = []
+# total_pred_tuned = []
+# total_outputs_tuned = []
+# with torch.no_grad():
+#     for data in tqdm(test_loader, desc="Pre-trained Model Evaluation"):
+#         human_input, gpt_output, images, visual_exps, class_ids, ans = data
+#         images = images.to(device)
+#         labels = class_ids.to(device)
+#         outputs, y_pred_text = model(images)
+#         _, predicted = torch.max(outputs.data, 1)
+#         total += labels.size(0)
+#         correct += (predicted == labels).sum().item()
+#         total_label_tuned.extend(labels.cpu().tolist())
+#         total_pred_tuned.extend(predicted.cpu().tolist())
+#         total_outputs_tuned.extend(outputs.cpu().tolist())
+
+# total_outputs_tuned = np.array(total_outputs_tuned)
+# total_label_tuned = np.array(total_label_tuned)
+
+# test_accuracy_tuned = accuracy_score(total_label_tuned, total_pred_tuned)
+# test_recall_tuned_micro = recall_score(total_label_tuned, total_pred_tuned, average="micro")
+# test_precision_tuned_micro = precision_score(total_label_tuned, total_pred_tuned, average="micro")
+# test_f1_tuned_micro = f1_score(total_label_tuned, total_pred_tuned, average="micro")
+
+# soft_max_outputs_tuned = torch.tensor(total_outputs_tuned)
+# soft_max_outputs_tuned = F.softmax(soft_max_outputs_tuned)
+
+# test_auc_tuned_micro = roc_auc_score(total_label_tuned, soft_max_outputs_tuned.numpy(), 
+#                                      average="micro", multi_class="ovr")
+
+# test_recall_tuned_macro = recall_score(total_label_tuned, total_pred_tuned, average="macro")
+# test_precision_tuned_macro = precision_score(total_label_tuned, total_pred_tuned, average="macro")
+# test_f1_tuned_macro = f1_score(total_label_tuned, total_pred_tuned, average="macro")
+# test_auc_tuned_macro = roc_auc_score(total_label_tuned, soft_max_outputs_tuned.numpy(), 
+#                                      average="macro", multi_class="ovr")
+
+# print("-"*10 + "Before Fine-tuning" + "-"*10)
+# tb = pt.PrettyTable()
+# tb.field_names = ["", "Accuracy", "Recall", "Precision", "F1", "AUC"]
+# tb.add_row(
+#     ["Micro",test_accuracy_tuned, test_recall_tuned_micro, 
+#      test_precision_tuned_micro, test_f1_tuned_micro, test_auc_tuned_micro]
+# )
+# tb.add_row(
+#     ['Macro', test_accuracy_tuned, test_recall_tuned_macro, 
+#      test_precision_tuned_macro, test_f1_tuned_macro, test_auc_tuned_macro]
+# )
+# print(tb)
+
+print("-"*10 + "Training Starts" + "-"*10)
+for epoch in range(args.num_epochs):
+    start_time = time.time()
     model.train()
-    total_loss = 0
-    for data in dataloader:
-        images = data['image'].to(device)
-        labels = data['class_id'].to(device)
+    running_loss = 0.0
+    running_pred_loss = 0.0
+    running_att_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        human_input, gpt_output, images, visual_exps, class_ids, ans = data
+        images = images.to(device)
+        class_ids = class_ids.to(device)
+        y_pred_label, y_pred_text = model(images)
+        pred_loss = criterion(y_pred_label, class_ids)
 
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
+        if args.explanation_type in ["multimodal", "visual"]:
+            attn_weights = model.get_attention_map() # torch.Size([16, 50, 50])
+            visual_exps_resized = resize_vit_att(visual_exps, attn_weights.shape[1], attn_weights.shape[2]).to(device)
+            att_loss = F.l1_loss(visual_exps_resized, attn_weights, reduction='mean') * 100 # Original att loss
+
+            # Transform att loss
+            visual_exp_resized_trans = visual_exp_transform(visual_exps_resized, args.transformation)
+            att_loss = att_loss + cal_trans_att_loss(attn_weights, visual_exp_resized_trans, args.transformation)
+        else:
+            att_loss = torch.zeros(1, dtype=torch.float32).to(device)
         
-        # Backward and optimize
+        scheduler.step()
+
+        loss = pred_loss + args.att_weight * att_loss
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
         # Accumulate losses
-        total_loss += loss.item()
+        running_loss += loss.item()
+        running_pred_loss += pred_loss.item()
+        running_att_loss += att_loss.item()
 
-    # Calculate average loss
-    avg_total_loss = total_loss / len(dataloader)
-    
-    return avg_total_loss
-ges = data['image'].to(device)
-    
-
-def test(model, dataloader, criterion, device):
-    model.eval()  # Set the model to evaluation mode
-    total_loss = 0
-    correct = 0
-    total = 0
-    
-    with torch.no_grad():  # Disable gradient computation
-        for data in dataloader:
-            images = data['image'].to(device)
-            labels = data['class_id'].to(device)
-            
-            # Forward pass
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            total_loss += loss.item()
-            
-            # Calculate accuracy
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    
-    avg_loss = total_loss / len(dataloader)
-    accuracy = 100 * correct / total
-    return avg_loss, accuracy
+        if (i + 1) % 5 == 0:
+            end_time = time.time()
+            print(f'[{epoch + 1} / {args.num_epochs}, {i + 1} / {len(train_loader)}] loss: {running_loss / 100:.3f}, '
+                  f'pred_loss: {running_pred_loss / 100:.3f}, '
+                  f'att_loss: {running_att_loss / 100:.3f}, time:{end_time - start_time:.3f}')
+            running_loss = 0.0
+            running_pred_loss = 0.0
+            running_att_loss = 0.0
 
 
-num_epochs = args.num_epochs
-# Training the model
-for epoch in range(num_epochs):
-    avg_total_loss = train(model_cls, train_loader, optimizer, criterion, device)
-    print(f'Epoch [{epoch+1}/{num_epochs}], Total Loss: {avg_total_loss:.4f}')
 
-test_loss, test_accuracy = test(model_cls, test_loader, criterion, device)
-print(f'Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.2f}%')
+model.eval()
+correct = 0
+total = 0
+total_label_tuned = []
+total_pred_tuned = []
+total_outputs_tuned = []
+with torch.no_grad():
+    for data in tqdm(test_loader, desc="Fine-tuned Model Evaluation"):
+        human_input, gpt_output, images, visual_exps, class_ids, ans = data
+        images = images.to(device)
+        labels = class_ids.to(device)
+        outputs, y_pred_text = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        total_label_tuned.extend(labels.cpu().tolist())
+        total_pred_tuned.extend(predicted.cpu().tolist())
+        total_outputs_tuned.extend(outputs.cpu().tolist())
 
+total_outputs_tuned = np.array(total_outputs_tuned)
+total_label_tuned = np.array(total_label_tuned)
 
+test_accuracy_tuned = accuracy_score(total_label_tuned, total_pred_tuned)
+test_recall_tuned_micro = recall_score(total_label_tuned, total_pred_tuned, average="micro")
+test_precision_tuned_micro = precision_score(total_label_tuned, total_pred_tuned, average="micro")
+test_f1_tuned_micro = f1_score(total_label_tuned, total_pred_tuned, average="micro")
+
+soft_max_outputs_tuned = torch.tensor(total_outputs_tuned)
+soft_max_outputs_tuned = F.softmax(soft_max_outputs_tuned)
+
+test_auc_tuned_micro = roc_auc_score(total_label_tuned, soft_max_outputs_tuned.numpy(), 
+                                     average="micro", multi_class="ovr")
+
+test_recall_tuned_macro = recall_score(total_label_tuned, total_pred_tuned, average="macro")
+test_precision_tuned_macro = precision_score(total_label_tuned, total_pred_tuned, average="macro")
+test_f1_tuned_macro = f1_score(total_label_tuned, total_pred_tuned, average="macro")
+test_auc_tuned_macro = roc_auc_score(total_label_tuned, soft_max_outputs_tuned.numpy(), 
+                                     average="macro", multi_class="ovr")
+
+print("-"*10 + "After Fine-tuning" + "-"*10)
+tb = pt.PrettyTable()
+tb.field_names = ["", "Accuracy", "Recall", "Precision", "F1", "AUC"]
+tb.add_row(
+    ["Micro",test_accuracy_tuned, test_recall_tuned_micro, 
+     test_precision_tuned_micro, test_f1_tuned_micro, test_auc_tuned_micro]
+)
+tb.add_row(
+    ['Macro', test_accuracy_tuned, test_recall_tuned_macro, 
+     test_precision_tuned_macro, test_f1_tuned_macro, test_auc_tuned_macro]
+)
+print(tb)
