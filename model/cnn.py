ee9a708ad69317ce400c7f82783155ecaf27b5da
Tianxu Jiang <88474547+TnTerry@users.noreply.github.com>
2024-08-02 06:06:12 -0400

    Update cnn.py

--- model/cnn.py
+++ model/cnn.py
@@ -1,8 +1,12 @@
+import sys
+import os
+
 import torch
 import torch.nn as nn
 from torchvision import transforms, models
-from MEGL.utils.utils import *
-from MEGL.utils.gradcam import GradCam
+
+from model.utils.utils import *
+from model.utils.gradcam import GradCam
 
 class MEGL_CNN(nn.Module):
     def __init__(self, explanation_type:str, num_classes:int) -> None:
@@ -10,11 +14,16 @@ class MEGL_CNN(nn.Module):
         self.explanation_type = explanation_type
         self.cnn_net = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
         self.cnn_net.fc = nn.Linear(self.cnn_net.fc.in_features, num_classes)
+        # TODO: LLaVA Init
         
     def forward(self, inputs:torch.Tensor) -> torch.Tensor:
-        if self.explanation_type in ["none", "visual"]:
-            y = self.cnn_net(inputs)
+        if self.explanation_type in ['none', 'visual']:
+            y_label = self.cnn_net(inputs)
+            y_text = None
         
         # TODO: CNN + LLaVA code
+        elif self.explanation_type in ['text', 'multimodal']:
+            y_label = self.cnn_net(inputs)
+            y_text = None
 
-        return y
+        return y_label, y_text
