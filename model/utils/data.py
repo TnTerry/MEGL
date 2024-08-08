06d4deb2febdfed24d9c237b7cbe539b30b4f49d
Tianxu Jiang <88474547+TnTerry@users.noreply.github.com>
2024-08-07 21:55:03 -0400

    Update data.py

--- model/utils/data.py
+++ model/utils/data.py
@@ -4,8 +4,10 @@ import json
 from dataclasses import dataclass
 
 import pandas as pd
+import numpy as np
 import torch
 from torch.utils.data import Dataset
+from torchvision import transforms
 from transformers import AutoProcessor
 
 class MEGLDataset(Dataset):
@@ -71,7 +73,15 @@ class QaImageOutput:
     pixel_values: torch.Tensor
     a_inputs_ids: torch.Tensor
 
-def build_qaimage(processor:AutoProcessor, q_text:str, a_text:str, image_path:Path):
+def build_qaimage(processor:AutoProcessor, q_text:str, a_text:str, image_path:Path, visual_exp_path:Path|None):
+
+    chat_template = """
+    "USER: <image>\n<prompt> ASSISTANT:"
+    """
+
+    # Set the chat template
+    processor.tokenizer.chat_template = chat_template
+
     # QA + instructions
     messages = [
     {"role": "system", "content": "You are a helpful assistant."},
@@ -83,7 +93,19 @@ def build_qaimage(processor:AutoProcessor, q_text:str, a_text:str, image_path:Pa
 
     # Image
     raw_image = Image.open(image_path)
-    inputs = processor(prompt, raw_image, return_tensors="pt")
+
+    if visual_exp_path:
+        visual_exp = np.load(visual_exp_path)
+        if visual_exp.ndim == 3 and visual_exp.shape[0] == 3:  # Check if it's a 3-channel image
+            visual_exp = visual_exp.transpose(1, 2, 0)  # Reorder dimensions to (height, width, channels)
+            if visual_exp.dtype == np.float64:
+                visual_exp = (visual_exp * 255).astype(np.uint8)  # Assuming the data is in range [0, 1]
+        visual_exp = Image.fromarray(visual_exp.astype('uint8'))  # Assume it's grayscale
+        visual_exp = visual_exp.convert('RGB')
+    else:
+        visual_exp = Image.new('RGB', raw_image.size, (0, 0, 0)) # Dummy image of zeros
+
+    inputs = processor(prompt, visual_exp, return_tensors="pt")
 
     a_inputs_ids = processor.tokenizer(
         a_text, 
@@ -97,6 +119,17 @@ def build_qaimage(processor:AutoProcessor, q_text:str, a_text:str, image_path:Pa
         pixel_values=inputs['pixel_values'],
         a_inputs_ids=a_inputs_ids
     )
+
+transform_image = transforms.Compose([
+                  transforms.Resize((256, 256)),
+                  transforms.ToTensor(),
+                  transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                  ]) 
+        
+transform_visualexp = transforms.Compose([
+                      transforms.Resize((256, 256)),
+                      transforms.ToTensor()
+                      ])
 
 class TrainMEGLCollator:
     def __init__(self, processor:AutoProcessor, INGNORE_INDEX:int) -> None:
@@ -135,7 +168,8 @@ class TrainMEGLCollator:
                 processor=self.processor,
                 q_text=feature[0],
                 a_text=feature[1],
-                image_path=feature[2]
+                image_path=feature[2],
+                visual_exp_path=feature[3]
             )
             tmp_input_ids, tmp_labels = self.convert_one_piece(
                 q_input_ids=qaimage_output.q_inputs_ids,
@@ -182,9 +216,44 @@ class TrainMEGLCollator:
         attention_mask = torch.ones_like(final_input_ids)
         attention_mask[final_input_ids ==
                        self.processor.tokenizer.pad_token_id] = 0
+
+        visual_exp_lst = []
+        image_lst = []
+        class_id_lst = []
+
+        for feature in features:
+            image = Image.open(feature[2]).convert('RGB')
+            image = transform_image(image)
+            image_lst.append(image)
+
+            if feature[3]:
+                visual_exp = np.load(feature[3])
+                if visual_exp.ndim == 3 and visual_exp.shape[0] == 3:  # Check if it's a 3-channel image
+                    visual_exp = visual_exp.transpose(1, 2, 0)  # Reorder dimensions to (height, width, channels)
+                    if visual_exp.dtype == np.float64:
+                        visual_exp = (visual_exp * 255).astype(np.uint8)  # Assuming the data is in range [0, 1]
+                
+                visual_exp = Image.fromarray(visual_exp.astype('uint8'))  # Assume it's grayscale
+                visual_exp = visual_exp.convert('RGB')
+            else:
+                visual_exp = Image.new('RGB', image.size, (0, 0, 0)) # Dummy image of zeros
+            
+            visual_exp = transform_visualexp(visual_exp)
+            visual_exp_lst.append(visual_exp)
+
+            class_id = torch.tensor(class_id, dtype=torch.long)
+            class_id_lst.append(class_id)
+        
+        images = torch.stack(image_lst)
+        visual_exps = torch.stack(visual_exp_lst)
+        class_ids = torch.tensor(class_id_lst)
+
         return {
             "input_ids": final_input_ids,
             "labels": final_labels,
             "pixel_values": final_pixel_values,
-            "attention_mask": attention_mask
+            "attention_mask": attention_mask,
+            "images":images,
+            "visual_exps":visual_exps,
+            "class_ids":class_ids
         }
